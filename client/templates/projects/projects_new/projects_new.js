3dbba70f3ab11ce848e9ca6e589325cdc9308710
pasharayan <pasha.rayan@gmail.com>
2015-10-24 22:09:35 +1100

    added projets new

--- client/templates/projects/projects_new/projects_new.js
+++ client/templates/projects/projects_new/projects_new.js
@@ -4,6 +4,26 @@ Template.projectsNew.helpers({
 
 Template.projectsNew.events({
 
+  'submit .new-project': function(e){
+    e.preventDefault();
+    var data = {
+      name: $(e.target).find('[name=name]').val(),
+      thumbnailUrl: $(e.target).find('[name=thumbnailUrl]').val(),
+      tagline: $(e.target).find('[name=tagline]').val(),
+      story: $(e.target).find('[name=story]').val(),
+      latitude: $(e.target).find('[name=latitude]').val(),
+      longitutde: $(e.target).find('[name=longitude]').val(),
+      goal: $(e.target).find('[name=goal]').val(),
+    };
+
+
+
+    var projectId = Projects.insert(data);
+    Router.go('projectsShow', {_id: projectId});
+
+
+  }
+
 });
 
 Template.projectsNew.created = function(){
